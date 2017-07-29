0eb26b844a9d4b7d07238f0021c7f3b9af0522bd
ankushkawanpure <ankushkawanpure@gmail.com>
2017-07-29 19:50:28 +0530

    material ui added

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -2,20 +2,26 @@
  * Created by ankush on 7/29/17.
  */
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Reacttapeventplugin from 'react-tap-event-plugin';
+
+Reacttapeventplugin();
 
 export default class Template extends  React.Component {
 
     render() {
         return(
-            <div>
-                <header>
-                    Ankush Kawanpure
-                </header>
+            <MuiThemeProvider>
+                <div>
+                    <header>
+                        Ankush Kawanpure
+                    </header>
 
-                <main>
-                    {this.props.children}
-                </main>
-            </div>
+                    <main>
+                        {this.props.children}
+                    </main>
+                </div>
+            </MuiThemeProvider>
         );
     }
 
